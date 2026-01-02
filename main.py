e71cb2b3c1e7bf96b13cc7a4c2b26a5fe03ca915
rivasreal <alibagci608@gmail.com>
2026-01-02 12:47:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import discord
+from discord.ext import commands
+from flask import Flask
+from threading import Thread
 
+# 7/24 Aktif tutmak için gerekli sistem
+app = Flask('')
+@app.route('/')
+def home(): return "Bot Aktif!"
+def run(): app.run(host='0.0.0.0', port=8080)
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} Başlatıldı!')
+
+@bot.command()
+async def parlat(ctx):
+    await ctx.send("✨ GEZGİN parlatma sistemi aktif!")
+
+keep_alive()
+# Aşağıdaki tırnak içine Discord Developer Portal'dan aldığın TOKENİ yapıştır!
+bot.run('MTQ1NTk3NDM5NTgyOTk0ODU3Nw.GSwY9N.muwvplglVVcuXgV7YR4xPeXx0Buedijz-0MEJU')
